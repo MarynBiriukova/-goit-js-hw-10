5799d990afa88e9043ade3b2b18c2bd809c40bae
Maryna <marbir66@gmail.com>
2026-07-16 12:10:34 +0300

    HW10_all1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,56 @@
-console.log("Snackbar");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import '../css/styles.css';
+
+
+
+iziToast.settings({
+  position: "topRight",
+  messageColor: "#ffffff",
+  iconColor: "#ffffff",
+  borderRadius: "8px",
+  theme: "dark",
+  progressBar: false,
+});
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (evt) => {
+    
+    event.preventDefault();
+
+    const delay = Number(evt.currentTarget.elements.delay.value);
+    const state = evt.currentTarget.elements.state.value;
+
+    const result = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') resolve(delay);
+            else reject(delay);
+
+    
+            result
+                .then((data) => {
+                    //console.log(`✅ Fulfilled promise in ${data}ms`);
+                    iziToast.success({
+                        message: `Fulfilled promise in ${data}ms`,
+                        backgroundColor: "#59b17a",
+                        });
+            
+                })
+                .catch((data) => {
+                    //console.log(`❌ Rejected promise in ${data}ms`);
+                   iziToast.error({
+                        message: `Rejected promise in ${data}ms`, 
+                        backgroundColor: "#ef4040",
+                        });
+                })
+
+
+        }, delay);
+
+        form.reset();
+    
+    });
+})
